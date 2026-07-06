114ca1e632c95b4c78137135f68a6015dc0caf71
James Wol <james.wol@petalmail.com>
2026-07-06 14:46:03 +0300

    Create engine/generator.py

---
+++ engine/generator.py
@@ -0,0 +1,28 @@
+import random
+from core.utils import uid
+from core.config import ARCH_DOMAINS
+
+def get_domain(btype):
+    for d, items in ARCH_DOMAINS.items():
+        if btype in items:
+            return d
+    return "Unknown"
+
+def generate_base_design(btype, bedrooms):
+    rooms = ["Living", "Kitchen", "Bathroom"] + ["Flex"] * random.randint(1, 3)
+
+    area = 65 + 44 + (bedrooms * 18)
+
+    return {
+        "id": uid(),
+        "type": btype,
+        "domain": get_domain(btype),
+        "bedrooms": bedrooms,
+        "rooms": rooms,
+        "area_sqm": area,
+        "structure": {
+            "columns": random.randint(14, 36),
+            "beams": random.randint(28, 72)
+        },
+        "cost": 0
+    }
