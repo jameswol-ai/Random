56d0306c5eb3a730ec012c77f5fcb5575d6e05c1
James Wol <james.wol@petalmail.com>
2026-07-19 00:40:36 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false,  // force light mode
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ff4b4b',
+        background: '#ffffff',
+        secondaryBg: '#f0f2f6',
+        text: '#262730',
+        border: '#e6e9ef',
+      },
+    },
+  },
+  plugins: [],
+}
